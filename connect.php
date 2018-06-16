5c9cc5586558e93777817f614bb28dd36ce10d80
zamoranorena <zamoranorena@gmail.com>
2018-06-15 19:31:17 -0500

    change2
    asdasd

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	$client = new Client(); // Instance Guzzle
-	$shopify_api_key = ''; // Api key 
-	$shopify_api_secret= '';
-	$shopify_store = 'mystore.shopify.com'
+    $client = new Client(); // Instance Guzzle
+    $shopify_api_key = ''; // Api key 
+    $shopify_api_secret= '';
+    $shopify_store = 'mystore.shopify.com'
     $string_json = array ( "product"  => array ( "title" => 'new product' , 'description' => 'my description' , 'tags' => 'programmer11'));
     $string_json = json_encode($string_json);
     $data = $client->post('https://'.$shopify_api_key.':'.$shopify_api_secret.'@'.$shopify_store.'/admin/products.json', null, $string_json);
